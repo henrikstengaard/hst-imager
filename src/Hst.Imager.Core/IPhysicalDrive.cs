e61d608750f171d44278d23560871cf8a8f634b3
Henrik Nørfjand Stengaard <hst@firstrealize.com>
2022-09-20 19:47:53 +0200

    Rename projects to hst imager

---
+++ src/Hst.Imager.Core/IPhysicalDrive.cs
@@ -0,0 +1,17 @@
+﻿namespace HstWbInstaller.Imager.Core
+{
+    using System.IO;
+
+    public interface IPhysicalDrive
+    {
+        string Path { get; }
+        string Type { get; }
+        string Model { get; }
+        long Size { get; }
+        bool Writable { get; }
+        //RigidDiskBlock RigidDiskBlock { get; set; }
+
+        Stream Open();
+        void SetWritable(bool writable);
+    }
+}
---
+++ src/Hst.Imager.Core/IPhysicalDriveManager.cs
@@ -0,0 +1,11 @@
+﻿namespace HstWbInstaller.Imager.Core
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public interface IPhysicalDriveManager
+    {
+        Task<IEnumerable<IPhysicalDrive>> GetPhysicalDrives();
+    }
+}
