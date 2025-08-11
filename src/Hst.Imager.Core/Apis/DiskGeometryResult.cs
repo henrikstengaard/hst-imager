61ab37070cafb094298e276addb7d07499e5faf5
Henrik Nørfjand Stengaard <hst@firstrealize.com>
2025-08-11 12:43:10 +0200

    Add support for floppy drives on windows currently disabled and will be investigated further in future

---
+++ src/Hst.Imager.Core/Apis/DiskGeometryResult.cs
@@ -0,0 +1,10 @@
+﻿namespace Hst.Imager.Core.Apis;
+
+public class DiskGeometryResult
+{
+    public string MediaType { get; set; }
+    public long Cylinders { get; set; }
+    public uint TracksPerCylinder { get; set; }
+    public uint SectorsPerTrack { get; set; }
+    public uint BytesPerSector { get; set; }
+}
