cc264257fd63a7c4ac9961dd1b3cf672c0a794c9
Henrik Nørfjand Stengaard <hst@firstrealize.com>
2025-11-13 21:31:51 +0100

    Fix path helper unit tests

--- src/Hst.Imager.Core/Helpers/PathHelper.cs
+++ src/Hst.Imager.Core/Helpers/PathHelper.cs
@@ -1,3 +1,5 @@
+using Hst.Imager.Core.Commands;
+
 namespace Hst.Imager.Core.Helpers
 {
     using System;
@@ -13,14 +15,29 @@ namespace Hst.Imager.Core.Helpers
         public static string GetFullPath(string path)
         {
             path = ResolveUserProfilePath(path);
-            if (Path.IsPathRooted(path))
+            
+            var isMacOsOrLinux = OperatingSystem.IsMacOS() || OperatingSystem.IsLinux(); 
+            
+            // return path, if not macos or linux and path is full path
+            if (!isMacOsOrLinux && path.Length > 0 && path.StartsWith("/"))
             {
                 return path;
             }
 
+            // return path, if not windows and path is full path
+            if (!OperatingSystem.IsWindows() && path.Length > 1 &&
+                (path.StartsWith(@"\") || Regexs.WindowsDriveRegex.IsMatch(path)))
+            {
+                return path;
+            }
+
+            // split path into directory and filename
             var dirName = Path.GetDirectoryName(path) ?? string.Empty;
             var fileName = Path.GetFileName(path) ?? string.Empty;
 
+            // get full path for directory and combine with filename
+            // main reason to not get full path for path, is because Windows 10
+            // return "\\.\AUX" when path ends with filename "AUX".
             return Path.Combine(Path.GetFullPath(string.IsNullOrEmpty(dirName)
                 ? Directory.GetCurrentDirectory() : dirName), fileName);
         }
