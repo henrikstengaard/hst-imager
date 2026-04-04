a4441173343713f52b7eb2b9b98b31fed30667a8
Henrik Nørfjand Stengaard <hst@firstrealize.com>
2026-04-04 22:45:00 +0200

    Update build and release github action to read version from directory build props

--- src/publish-console-app.ps1
+++ src/publish-console-app.ps1
@@ -1,9 +1,24 @@
-# update neutralinojs
+param (
+    [string]$target
+)
+
+$targets = if ($target) { @($target) } else { @('win-x64', 'win-x86', 'win-arm64', 'osx-x64', 'osx-arm64', 'linux-x64', 'linux-arm', 'linux-arm64') }
+
+$commitCount = (git rev-list --count HEAD)
+$buildVersion = (Select-Xml -Path ./Directory.Build.props -XPath '/Project/PropertyGroup/Version').Node.InnerXML
+
+$version = $buildVersion -replace '^(.*)\.\d+.*$', "`$1.$commitCount"
+$assemblyVersion = $buildVersion -replace '^(.*)\.\d+.*$', "`$1.$commitCount.0"
+
 Push-Location 'Hst.Imager.ConsoleApp'
 
-foreach ($target in @('win-x64', 'win-x86', 'win-arm64', 'osx-x64', 'osx-arm64', 'linux-x64', 'linux-arm', 'linux-arm64'))
+foreach ($target in $targets)
 {
-    dotnet publish --configuration Release -p:PublishSingleFile=True -p:SelfContained=True -p:RuntimeIdentifier=$target -p:PublishReadyToRun=True -p:IncludeNativeLibrariesForSelfExtract=true -p:PublishDir=publish/$target
+	Write-Host "Building target: $target"
+	Write-Host "Version: $version"
+	Write-Host "Assembly version: $assemblyVersion"
+	
+	dotnet publish --configuration Release -p:PublishSingleFile=True -p:SelfContained=True -p:RuntimeIdentifier=$target -p:PublishReadyToRun=True -p:IncludeNativeLibrariesForSelfExtract=true -p:PublishDir=publish/$target -p:Version=$version -p:AssemblyVersion=$assemblyVersion -p:FileVersion=$assemblyVersion -p:PackageVersion=$assemblyVersion
 }
 
 Pop-Location
