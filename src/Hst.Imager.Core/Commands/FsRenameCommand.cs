a12455d59125012a6452a4ec93ef1e04442f20fe
Henrik Nørfjand Stengaard <hst@firstrealize.com>
2026-06-30 22:32:31 +0200

    Add initial fs rename command

---
+++ src/Hst.Imager.Core/Commands/FsRenameCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Hst.Core;
+using Hst.Imager.Core.UaeMetadatas;
+using Microsoft.Extensions.Logging;
+
+namespace Hst.Imager.Core.Commands;
+
+public class FsRenameCommand(
+    ILogger<FsRenameCommand> logger,
+    ICommandHelper commandHelper,
+    IEnumerable<IPhysicalDrive> physicalDrives,
+    string fromPath,
+    string toPath,
+    UaeMetadata uaeMetadata = UaeMetadata.UaeFsDb)
+    : FsCommandBase(commandHelper, physicalDrives)
+{
+    private readonly ILogger<FsRenameCommand> logger = logger;
+    public override Task<Result> Execute(CancellationToken token)
+    {
+        throw new NotImplementedException();
+    }
+}
